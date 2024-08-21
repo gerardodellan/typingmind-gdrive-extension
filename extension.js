5abcf45651d0e77b6358aef30f4cdc715f44f8fd
gerardodellan <140569859+gerardodellan@users.noreply.github.com>
2024-08-21 14:22:29 -0600

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,1 +1,28 @@
+class GoogleDriveSync {
+  constructor() {
+    this.CLIENT_ID = 'YOUR_CLIENT_ID_HERE'; // You'll fill this in later
+    this.API_KEY = 'YOUR_API_KEY_HERE'; // You'll fill this in later
+    this.SCOPES = 'https://www.googleapis.com/auth/drive.file';
+    console.log("Google Drive Sync extension initialized");
+  }
 
+  onload() {
+    console.log("Google Drive Sync extension loaded");
+    this.loadGoogleApiScript();
+  }
+
+  loadGoogleApiScript() {
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/api.js';
+    script.onload = () => this.initializeGoogleApi();
+    document.body.appendChild(script);
+  }
+
+  initializeGoogleApi() {
+    console.log("Google API script loaded");
+    // We'll implement Google API initialization in the next step
+  }
+}
+
+// Register the extension
+window.TypingMindExtension = GoogleDriveSync;
